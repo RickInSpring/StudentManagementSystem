c5347f7bdd1f1213e2b1098333d2f7b8365c3eb4
RickInSpring <mukherjeesoumyajit308@gmail.com>
2025-09-06 02:44:33 +0530

    initial commit

---
+++ StudentManagementSystemdemo/src/test/java/pcsglobalcom/example/StudentManagementSystemdemo/StudentManagementSystemdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pcsglobalcom.example.StudentManagementSystemdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
